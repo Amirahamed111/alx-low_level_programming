3443609c8b4f7eeee2c17dc9ef3046e5e98c5477
Amirahamed111 <amirhamed2274@gmail.com>
2023-07-19 16:38:54 -0700

    c func all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
  * main - Entry point
  * Description: "putchar"
  * Return:  0 (Success)
  */
-
 int main(void)
 {
-        char str[9] = "_putchar";
-        int p = 0;
+	char str[9] = "_putchar";
+	int i = 0;
 
-        while (p < 8)
-        {
-                _putchar(str[p]);
-                p++;
-        }
-        _putchar('\n');
-        return (0);
+	while (i < 8)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
